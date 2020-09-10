5f09b2e786ff755b9c8a6f68ce43b0bb7774c079
niloyzaman <niloy.zaman13@gmail.com>
2020-09-10 11:38:34 -0400

    first commit

---
+++ data/index.js
@@ -0,0 +1,129 @@
+export const defaultTheme = 'dark'; // light
+
+export const Title = 'Niloy Zaman ~ QA Automation Engineer';
+
+export const Resume = {
+    introduction: `{
+        Introduction = () => {
+            Namaste: {
+                Name: 'Niloy Zaman',
+                Designation: 'QA Automation Engineer'
+            }
+        }
+}`,
+
+    experience: `{
+        Experience = () => {
+            BOF: {
+                company: 'Bankf Of America',
+                designation: 'QA Automation Engineer',
+                period: 'June 2018 to Present'
+            },
+            Everyday: {
+                company: 'Everyday Health Group',
+                designation: 'QA Engineer',
+                period: 'April 2016 to May 2018'
+            },
+            Metroplus: {
+                company: 'Metroplus Health ',
+                designation: 'QA Engineer',
+                period: 'February 2015 to March 2016'
+            },
+
+        }
+}`,
+
+    skills: `{
+        Skills = () => {
+            Testing Tools: {
+                Sekenium, WebDriver, Selenium Grid, Cucumber JVM,
+
+            },
+            Project Management Tools: {
+                HP Quality Center,
+                JIRA,
+                Rally,
+                HP ALM'
+            },
+            Languages: {
+                js: 'JavaScript',
+                Java,
+                SQL,
+                VB Script',
+                Gherkins,
+                Shell Script'
+                HTML,
+                XML,
+            },
+            Databases: {
+                MYSQL,
+                Toad,
+                MS SQL Server,
+                Oracle, Access,
+                MySQL Workbench,
+            },
+            IDE: {
+                Eclipse,
+                IntelliJ,
+
+            },
+            	Build Tool: {
+                Maven,
+
+            },
+              CI: 'Continuous Integration'{
+                Jenkins,
+                Cruise Control,
+                Hudson,
+            },
+              OP:'Operating Systems'{
+                Windows 7,
+                Windows 8,
+                Windows 10,
+                Windows server,
+                UNIX,
+            },
+              Methodology:{
+                Agile (SCRUM, XP),
+            },
+              VC:'Version Control'{
+                  GIT,
+                  GitHub,
+                  Bitbucket,
+              },
+            	Design pattern: {
+                Model View Controller (MVC),
+                Google Page Object,
+                Data Object,
+              },
+              TF:'Test Framework'{
+                Junit,
+                TestNg,
+              },
+              Reporting:{
+                SureFire,
+                Extent Reporting,
+              }
+        }
+}`,
+
+
+
+
+
+    contact: `{
+        Contact = () => {
+            {
+                email: 'niloy@nzaman.us',
+                mobile: '929-350-5817'
+            }
+        }
+}`
+};
+
+
+export const SocialLinks = {
+    resume: '/resume.pdf',
+    linkedin: 'https://www.linkedin.com/in/niloyzaman/',
+
+}
